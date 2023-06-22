534b1c998c3674b588ff509e8f3f45b1cc11e4cb
LaurelineAB <laureline.agabibrac@gmail.com>
2023-06-22 18:14:52 +0200

    Modifier les informations de l'utilisateur

--- assets/js/apps/users.js
+++ assets/js/apps/users.js
@@ -82,6 +82,7 @@ export default {
 
         modifyUser(user)
         {
+            //Créer la ligne d'inputs
             let tbody = document.querySelector("tbody");
             let row = document.createElement("tr");
             let index = parseInt(user.id);
@@ -89,27 +90,33 @@ export default {
             console.log(index);
             tbody.insertBefore(row,nextRow);
 
-            // let form = document.createElement("form");
+            //Faire disparaître le bouton Modifier
+            let modifyBtn = document.querySelector(`tbody tr:nth-of-type(${index}) .modify`);
+            modifyBtn.style.display = "none";
             let empty = document.createElement("td");
 
+            //Prénom
             let tdFirstName = document.createElement("td");
             let inputFirst = document.createElement("input");
             inputFirst.setAttribute("type","text");
             inputFirst.value = user.firstName;
             tdFirstName.appendChild(inputFirst);
 
+            //Nom
             let tdLastName = document.createElement("td");
             let inputLast = document.createElement("input");
             inputLast.setAttribute("type","text");
             inputLast.value = user.lastName;
             tdLastName.appendChild(inputLast);
 
+            //Email
             let tdEmail = document.createElement("td");
             let inputEmail = document.createElement("input");
             inputEmail.setAttribute("type","text");
             inputEmail.value = user.email;
             tdEmail.appendChild(inputEmail);
 
+            //Bouton Valider
             let tdBtn = document.createElement("td");
             let validateBtn = document.createElement("button");
             validateBtn.textContent = "Valider";
@@ -117,13 +124,22 @@ export default {
             validateBtn.classList.add("btn-success");
             tdBtn.appendChild(validateBtn);
 
-            // form.appendChild(tdFirstName);
+            //Injecter les nouvelles cases
             row.appendChild(empty);
             row.appendChild(tdFirstName);
             row.appendChild(tdLastName);
             row.appendChild(tdEmail);
             row.appendChild(tdBtn);
             
+            //Valider les nouvelles informations de l'utilisateur
+            validateBtn.addEventListener("click", function()
+            {
+                user.firstName = inputFirst.value;
+                user.lastName = inputLast.value;
+                user.email = inputEmail.value;
+                row.remove();
+                modifyBtn.style.display = "inline-block";
+            })
         }
     }
 }
