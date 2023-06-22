5376cda06f47774c9a5a3f98273955f52edfbd6e
LaurelineAB <laureline.agabibrac@gmail.com>
2023-06-22 17:21:51 +0200

    Création de la ligne pour modification d'utilisateur

--- assets/js/apps/users.js
+++ assets/js/apps/users.js
@@ -46,7 +46,7 @@ export default {
         changeStatus(event)
         //Sinon, mettre "user" en paramètre, "user" étant déjà défini dans la boucle html
         {
-            let buttons = document.querySelectorAll("tbody button");
+            let buttons = document.querySelectorAll("tbody tr button:first-of-type");
             let btnArray = Array.from(buttons);
             let index = btnArray.indexOf(event.target);
             for (let i=0; i<buttons.length; i++)
@@ -78,6 +78,52 @@ export default {
                 isActive: true
             };
             this.users.push(newUser);
+        },
+
+        modifyUser(user)
+        {
+            let tbody = document.querySelector("tbody");
+            let row = document.createElement("tr");
+            let index = parseInt(user.id);
+            let nextRow = document.querySelector(`tbody tr:nth-of-type(${index+1})`);
+            console.log(index);
+            tbody.insertBefore(row,nextRow);
+
+            // let form = document.createElement("form");
+            let empty = document.createElement("td");
+
+            let tdFirstName = document.createElement("td");
+            let inputFirst = document.createElement("input");
+            inputFirst.setAttribute("type","text");
+            inputFirst.value = user.firstName;
+            tdFirstName.appendChild(inputFirst);
+
+            let tdLastName = document.createElement("td");
+            let inputLast = document.createElement("input");
+            inputLast.setAttribute("type","text");
+            inputLast.value = user.lastName;
+            tdLastName.appendChild(inputLast);
+
+            let tdEmail = document.createElement("td");
+            let inputEmail = document.createElement("input");
+            inputEmail.setAttribute("type","text");
+            inputEmail.value = user.email;
+            tdEmail.appendChild(inputEmail);
+
+            let tdBtn = document.createElement("td");
+            let validateBtn = document.createElement("button");
+            validateBtn.textContent = "Valider";
+            validateBtn.classList.add("btn");
+            validateBtn.classList.add("btn-success");
+            tdBtn.appendChild(validateBtn);
+
+            // form.appendChild(tdFirstName);
+            row.appendChild(empty);
+            row.appendChild(tdFirstName);
+            row.appendChild(tdLastName);
+            row.appendChild(tdEmail);
+            row.appendChild(tdBtn);
+            
         }
     }
 }
