e3bf05c211bf377b7aa2b483368709581ad429d3
LaurelineAB <laureline.agabibrac@gmail.com>
2023-06-22 15:53:04 +0200

    Formatage

--- assets/js/apps/header.js
+++ assets/js/apps/header.js
@@ -1,8 +1,6 @@
 export default {  
     data() {  
-        //  return {
-        //     path: ""
-        //  }
+        
     },
     setup () {
 	    // le code sera exécuté à la création de l'APP
@@ -12,23 +10,8 @@ export default {
         return {
             path
         }
-        // if (path === "/home/laurelineaga-bibrac/js-vue-interface-admin/index.html")
-        // {
-        //     let home = document.querySelector("#header nav");
-        //     console.log(home);
-        //     home.classList.add("active");
-        // }
-        // else if (path === "/home/laurelineaga-bibrac/js-vue-interface-admin/users.html")
-        // {
-        //     let users =  document.querySelector("#header navbar-nav a:nth-of-type(2)");
-        //     users.classList.add("active");
-        // }
-        
     },  
     methods : {  
-        // getCurrentPage()
-        // {
-        //     this.path = window.location.pathname;
-        // }
+
     }  
 }
